c88084574a414466e849650d917c2b4335553e2c
Nathan850 <45705618+az567812az@users.noreply.github.com>
2022-10-23 10:04:51 +0800

    Create Sign_in_robot.py

---
+++ Sign_in_robot.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+driver_path = 'C:\Program Files\chromedriver.exe'
+
+browser = webdriver.Chrome(driver_path)
+
+browser.get('https://www.feastogether.com.tw/booking/2')
+
+time.sleep(0.5)
+
+close_button = browser.find_element(By.LINK_TEXT, "我知道了").click()
+
+Sign_in_botton = browser.find_element(By.XPATH, "//ul[@class='wk-member-menu clearfix']/li[2]").click()
+
+time.sleep(0.5)
+
+username = browser.find_element(By.XPATH, "//input[@placeholder='手機']").send_keys("*******")
+
+time.sleep(0.5)
+
+password = browser.find_element(By.XPATH, "//input[@type='password']").send_keys("*******")
+
+time.sleep(0.5)
+
+Sign_in = browser.find_element(By.NAME, "form-login").click()
+
+time.sleep(0.5)
+
+alert = browser.switch_to.alert.accept()
+
